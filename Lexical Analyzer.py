fb39705d7eb012813d08c965f3414a3d1d5e6eaa
Arie3s <touseef4430120@cloud.neduet.edu.pk>
2024-06-28 23:35:41 +0500

    Update Lexical Analyzer.py

--- Lexical Analyzer.py
+++ Lexical Analyzer.py
@@ -1,15 +1,18 @@
-import customtkinter as ctk
 import ply.lex as lex
+import customtkinter as ctk
 
-class Tokeniser:
-    tokens = (
-        'SELECT', 'UPDATE', 'CREATE', 'DROP', 'TABLE', 'ORDER', 'BY', 'FROM',
-        'INSERT', 'INTO', 'VALUES', 'WHERE', 'AND', 'OR', 'JOIN', 'ON',
-        'IDENTIFIER', 'NUMBER', 'STRING',
-        'ASTERISK', 'COMMA', 'SEMICOLON',
-        'EQUALS', 'LESS', 'GREATER', 'LESS_EQUAL', 'GREATER_EQUAL', 'NOT_EQUAL'
-    )
+class SimpleSQLTokenizer:
+    # Define tokens
+    tokens = [
+        'SELECT', 'UPDATE', 'CREATE', 'DROP', 'TABLE', 'ORDER', 'BY', 'FROM', 'INSERT',
+        'INTO', 'VALUES', 'WHERE', 'AND', 'OR', 'JOIN', 'ON', 'ASTERISK', 'COMMA',
+        'SEMICOLON', 'EQUALS', 'LESS', 'GREATER', 'LESS_EQUAL', 'GREATER_EQUAL',
+        'NOT_EQUAL', 'NUMBER', 'STRING', 'COMMENT', 'LPAREN', 'RPAREN',
+        'NULL', 'INT', 'NOT', 'DATETIME', 'PRIMARY', 'KEY', 'CHAR', 'IDENTITY',
+        'VARCHAR', 'ALTER', 'FOREIGN', 'IDENTIFIER'
+    ]
 
+    # Token definitions using regular expressions
     t_SELECT = r'SELECT'
     t_UPDATE = r'UPDATE'
     t_CREATE = r'CREATE'
@@ -35,51 +38,115 @@ class Tokeniser:
     t_LESS_EQUAL = r'<='
     t_GREATER_EQUAL = r'>='
     t_NOT_EQUAL = r'!='
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+    t_NULL = r'NULL'
+    t_INT = r'INT'
+    t_NOT = r'NOT'
+    t_DATETIME = r'DATETIME'
+    t_PRIMARY = r'PRIMARY'
+    t_KEY = r'KEY'
+    t_CHAR = r'CHAR'
+    t_IDENTITY = r'IDENTITY'
+    t_VARCHAR = r'VARCHAR'
+    t_ALTER = r'ALTER'
+    t_FOREIGN = r'FOREIGN'
 
-    t_IDENTIFIER = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    t_NUMBER = r'\d+'
-    t_STRING = r'\'[^\']*\''
+    # Regular expression rules for complex tokens
+    t_ignore = ' \t'  # Ignored characters (spaces and tabs)
 
-    t_ignore = ' \t'
+    def t_IDENTIFIER(self, t):
+        r'[A-Za-z_][A-Za-z0-9_]*'
+        if t.value.upper() in self.tokens_map:
+            t.type = self.tokens_map[t.value.upper()]
+        else:
+            t.type = 'IDENTIFIER'
+        return t
 
-    def __init__(self):
-        self.lexer = lex.lex(module=self)
+    def t_NUMBER(self, t):
+        r'\d+'
+        t.value = int(t.value)
+        return t
 
-    def input(self, sql_statement):
-        self.lexer.input(sql_statement)
+    def t_STRING(self, t):
+        r'\'[^\']*\''
+        t.value = t.value[1:-1]  # Remove quotes
+        return t
 
-    def analyse(self):
-        tokens = []
-        while True:
-            tok = self.lexer.token()
-            if not tok:
-                break
-            tokens.append(f"{tok.type}: {tok.value}")
-        return tokens
+    def t_COMMENT(self, t):
+        r'--.*'
+        pass  # Ignore comments
 
     def t_newline(self, t):
         r'\n+'
         t.lexer.lineno += len(t.value)
 
-    def t_COMMENT(self, t):
-        r'--.*'
-        pass
-
-    def t_MULTI_LINE_COMMENT(self, t):
-        r'/\*.*?\*/'
-        pass
-
     def t_error(self, t):
-        print(f"Illegal character '{t.value[0]}' at line {t.lexer.lineno}")
+        print(f"Illegal character '{t.value[0]}'")
         t.lexer.skip(1)
+
+    tokens_map = {
+        'SELECT': 'SELECT',
+        'UPDATE': 'UPDATE',
+        'CREATE': 'CREATE',
+        'DROP': 'DROP',
+        'TABLE': 'TABLE',
+        'ORDER': 'ORDER',
+        'BY': 'BY',
+        'FROM': 'FROM',
+        'INSERT': 'INSERT',
+        'INTO': 'INTO',
+        'VALUES': 'VALUES',
+        'WHERE': 'WHERE',
+        'AND': 'AND',
+        'OR': 'OR',
+        'JOIN': 'JOIN',
+        'ON': 'ON',
+        '*': 'ASTERISK',
+        ',': 'COMMA',
+        ';': 'SEMICOLON',
+        '=': 'EQUALS',
+        '<': 'LESS',
+        '>': 'GREATER',
+        '<=': 'LESS_EQUAL',
+        '>=': 'GREATER_EQUAL',
+        '!=': 'NOT_EQUAL',
+        'NULL': 'NULL',
+        'INT': 'INT',
+        'NOT': 'NOT',
+        'DATETIME': 'DATETIME',
+        'PRIMARY': 'PRIMARY',
+        'KEY': 'KEY',
+        'CHAR': 'CHAR',
+        'IDENTITY': 'IDENTITY',
+        'VARCHAR': 'VARCHAR',
+        'ALTER': 'ALTER',
+        'FOREIGN': 'FOREIGN'
+    }
+
+    def __init__(self):
+        # Build the lexer
+        self.lexer = lex.lex(module=self)
+
+    def tokenize(self, sql_statement):
+        # Tokenize the SQL statement
+        self.lexer.input(sql_statement)
+        tokens = []
+
+        while True:
+            tok = self.lexer.token()
+            if not tok:
+                break
+            tokens.append((tok.type, tok.value))
+
+        return tokens
 
 
 class App:
-
     def __init__(self):
-        self.tokeniser = Tokeniser()
+        self.tokenizer = SimpleSQLTokenizer()
         self.root = ctk.CTk()
-        self.root.title("SQL Tokenizer")
+        self.root.title("Simple SQL Tokenizer")
 
         # SQL input text area
         sql_label = ctk.CTkLabel(self.root, text="Enter SQL statement:")
@@ -105,10 +172,10 @@ class App:
 
     def tokenize_input(self):
         sql = self.sql_entry.get("1.0", "end-1c")
-        self.tokeniser.input(sql)
-        tokens = self.tokeniser.analyse()
+        tokens = self.tokenizer.tokenize(sql)
         self.output.delete("1.0", ctk.END)
-        self.output.insert(ctk.END, "\n".join(tokens))
+        for token_type, value in tokens:
+            self.output.insert(ctk.END, f"{token_type}: {value}\n")
 
     def clear_output(self):
         self.output.delete("1.0", ctk.END)
@@ -116,6 +183,7 @@ class App:
 
     def run(self):
         self.root.mainloop()
+
 
 # Run the GUI
 app = App()
