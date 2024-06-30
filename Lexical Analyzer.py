54653fc782fa3fc445ed925967709fbb550d161c
Arie3s <touseef4430120@cloud.neduet.edu.pk>
2024-06-30 19:09:24 +0500

    Update Lexical Analyzer.py
    changed token type to keyword
    Documented

--- Lexical Analyzer.py
+++ Lexical Analyzer.py
@@ -1,34 +1,52 @@
+#-------------------------------------------------------------------------------
+# Name:        SSQLT
+# Purpose:     Simple SQL Tokenizer with GUI
+#
+# Author:      Walit
+#
+# Created:     30/06/2024
+# Copyright:   (c) walit 2024
+# Licence:     MIT License
+#-------------------------------------------------------------------------------
+
 import ply.lex as lex
 import customtkinter as ctk
 
 class SimpleSQLTokenizer:
+    """
+    SimpleSQLTokenizer class uses PLY to tokenize SQL statements.
+
+    Attributes:
+        tokens (list): List of token types to be recognized by the lexer.
+        keywords (set): Set of SQL keywords to be recognized.
+        lexer (Lexer): PLY lexer instance for tokenizing input SQL statements.
+
+    Methods:
+        t_IDENTIFIER(t): Tokenizes identifiers and keywords.
+        t_INTEGER(t): Tokenizes integer literals.
+        t_STRING(t): Tokenizes string literals.
+        t_COMMENT(t): Ignores comments in the SQL input.
+        t_newline(t): Tracks new lines in the input for accurate line numbers.
+        t_error(t): Handles illegal characters in the input.
+        tokenize(sql_statement): Tokenizes the input SQL statement and returns a list of tokens.
+    """
+
     # Define tokens
     tokens = [
-        'SELECT', 'UPDATE', 'CREATE', 'DROP', 'TABLE', 'ORDER', 'BY', 'FROM', 'INSERT',
-        'INTO', 'VALUES', 'WHERE', 'AND', 'OR', 'JOIN', 'ON', 'ASTERISK', 'COMMA',
-        'SEMICOLON', 'EQUALS', 'LESS', 'GREATER', 'LESS_EQUAL', 'GREATER_EQUAL',
-        'NOT_EQUAL', 'NUMBER', 'STRING', 'COMMENT', 'LPAREN', 'RPAREN',
-        'NULL', 'INT', 'NOT', 'DATETIME', 'PRIMARY', 'KEY', 'CHAR', 'IDENTITY',
-        'VARCHAR', 'ALTER', 'FOREIGN', 'ADD', 'CONSTRAINT', 'UNIQUE', 'IDENTIFIER'
+        'KEYWORD', 'ASTERISK', 'COMMA', 'SEMICOLON', 'EQUALS', 'LESS',
+        'GREATER', 'LESS_EQUAL', 'GREATER_EQUAL', 'NOT_EQUAL', 'INTEGER',
+        'STRING', 'COMMENT', 'LPAREN', 'RPAREN', 'IDENTIFIER',
     ]
 
-    # Token definitions using regular expressions
-    t_SELECT = r'SELECT'
-    t_UPDATE = r'UPDATE'
-    t_CREATE = r'CREATE'
-    t_DROP = r'DROP'
-    t_TABLE = r'TABLE'
-    t_ORDER = r'ORDER'
-    t_BY = r'BY'
-    t_FROM = r'FROM'
-    t_INSERT = r'INSERT'
-    t_INTO = r'INTO'
-    t_VALUES = r'VALUES'
-    t_WHERE = r'WHERE'
-    t_AND = r'AND'
-    t_OR = r'OR'
-    t_JOIN = r'JOIN'
-    t_ON = r'ON'
+    # List of SQL keywords
+    keywords = {
+        'SELECT', 'UPDATE', 'CREATE', 'DROP', 'TABLE', 'ORDER', 'BY', 'FROM', 'INSERT',
+        'INTO', 'VALUES', 'WHERE', 'AND', 'OR', 'JOIN', 'ON', 'NULL', 'INT', 'NOT',
+        'DATETIME', 'PRIMARY', 'KEY', 'CHAR', 'IDENTITY', 'VARCHAR', 'ALTER', 'FOREIGN',
+        'ADD', 'CONSTRAINT', 'UNIQUE'
+    }
+
+    # Token definitions using regular expressions for symbols
     t_ASTERISK = r'\*'
     t_COMMA = r','
     t_SEMICOLON = r';'
@@ -40,40 +58,26 @@ class SimpleSQLTokenizer:
     t_NOT_EQUAL = r'!='
     t_LPAREN = r'\('
     t_RPAREN = r'\)'
-    t_NULL = r'NULL'
-    t_INT = r'INT'
-    t_NOT = r'NOT'
-    t_DATETIME = r'DATETIME'
-    t_PRIMARY = r'PRIMARY'
-    t_KEY = r'KEY'
-    t_CHAR = r'CHAR'
-    t_IDENTITY = r'IDENTITY'
-    t_VARCHAR = r'VARCHAR'
-    t_ALTER = r'ALTER'
-    t_FOREIGN = r'FOREIGN'
-    t_ADD = r'ADD'
-    t_CONSTRAINT = r'CONSTRAINT'
-    t_UNIQUE = r'UNIQUE'
 
-    # Regular expression rules for complex tokens
-    t_ignore = ' \t'  # Ignored characters (spaces and tabs)
+    t_ignore = ' \t'  # Ignored characters
 
     def t_IDENTIFIER(self, t):
         r'[A-Za-z_][A-Za-z0-9_]*'
-        if t.value.upper() in self.tokens_map:
-            t.type = self.tokens_map[t.value.upper()]
+        token_value_upper = t.value.upper()
+        if token_value_upper in self.keywords:
+            t.type = 'KEYWORD'  # Assign KEYWORD token type
         else:
             t.type = 'IDENTIFIER'
         return t
 
-    def t_NUMBER(self, t):
+    def t_INTEGER(self, t):
         r'\d+'
         t.value = int(t.value)
         return t
 
     def t_STRING(self, t):
         r'\'[^\']*\''
-        t.value = t.value[1:-1]  # Remove quotes
+        t.value = t.value[1:-1]  # Remove the surrounding quotes
         return t
 
     def t_COMMENT(self, t):
@@ -85,71 +89,63 @@ class SimpleSQLTokenizer:
         t.lexer.lineno += len(t.value)
 
     def t_error(self, t):
+        """
+        Handles illegal characters in the input.
+
+        Parameters:
+            t (Token): The illegal token encountered.
+        """
         print(f"Illegal character '{t.value[0]}'")
         t.lexer.skip(1)
 
-    tokens_map = {
-        'SELECT': 'SELECT',
-        'UPDATE': 'UPDATE',
-        'CREATE': 'CREATE',
-        'DROP': 'DROP',
-        'TABLE': 'TABLE',
-        'ORDER': 'ORDER',
-        'BY': 'BY',
-        'FROM': 'FROM',
-        'INSERT': 'INSERT',
-        'INTO': 'INTO',
-        'VALUES': 'VALUES',
-        'WHERE': 'WHERE',
-        'AND': 'AND',
-        'OR': 'OR',
-        'JOIN': 'JOIN',
-        'ON': 'ON',
-        '*': 'ASTERISK',
-        ',': 'COMMA',
-        ';': 'SEMICOLON',
-        '=': 'EQUALS',
-        '<': 'LESS',
-        '>': 'GREATER',
-        '<=': 'LESS_EQUAL',
-        '>=': 'GREATER_EQUAL',
-        '!=': 'NOT_EQUAL',
-        'NULL': 'NULL',
-        'INT': 'INT',
-        'NOT': 'NOT',
-        'DATETIME': 'DATETIME',
-        'PRIMARY': 'PRIMARY',
-        'KEY': 'KEY',
-        'CHAR': 'CHAR',
-        'IDENTITY': 'IDENTITY',
-        'VARCHAR': 'VARCHAR',
-        'ALTER': 'ALTER',
-        'FOREIGN': 'FOREIGN',
-        'ADD': 'ADD',
-        'CONSTRAINT': 'CONSTRAINT',
-        'UNIQUE': 'UNIQUE'
-    }
-
     def __init__(self):
-        # Build the lexer
+        """
+        Initializes the SimpleSQLTokenizer with a PLY lexer instance.
+        """
         self.lexer = lex.lex(module=self)
 
     def tokenize(self, sql_statement):
-        # Tokenize the SQL statement
+        """
+        Tokenizes the input SQL statement.
+
+        Parameters:
+            sql_statement (str): The SQL statement to be tokenized.
+
+        Returns:
+            list: A list of tuples where each tuple contains a token type and its value.
+        """
         self.lexer.input(sql_statement)
         tokens = []
 
         while True:
-            tok = self.lexer.token()
-            if not tok:
+            token = self.lexer.token()
+            if not token:
                 break
-            tokens.append((tok.type, tok.value))
+            tokens.append((token.type, token.value))
 
         return tokens
 
 
 class App:
+    """
+    App class provides a GUI for the SQL tokenizer using CustomTkinter.
+
+    Attributes:
+        tokenizer (SimpleSQLTokenizer): Instance of the SQL tokenizer.
+        root (CTk): The main window of the application.
+        sql_entry (CTkTextbox): Text area for inputting SQL statements.
+        output (CTkTextbox): Text area for displaying the tokenized output.
+
+    Methods:
+        tokenize_input(): Tokenizes the SQL statement from the input text area and displays the tokens.
+        clear_output(): Clears the input and output text areas.
+        run(): Starts the main event loop of the application.
+    """
+
     def __init__(self):
+        """
+        Initializes the App with a SimpleSQLTokenizer and sets up the GUI.
+        """
         self.tokenizer = SimpleSQLTokenizer()
         self.root = ctk.CTk()
         self.root.title("Simple SQL Tokenizer")
@@ -163,15 +159,15 @@ class App:
 
         # Frame for buttons
         button_frame = ctk.CTkFrame(self.root)
-        button_frame.pack(padx=10, pady=(5, 0), anchor='w',fill='x')
+        button_frame.pack(padx=10, pady=(5, 0), anchor='w', fill='x')
 
         # Tokenize button
-        tokenize_button = ctk.CTkButton(button_frame, text="Tokenize", command=self.tokenize_input,height=40)
-        tokenize_button.pack(side='left', padx=(0, 10), pady=5,fill='x',expand='true')
+        tokenize_button = ctk.CTkButton(button_frame, text="Tokenize", command=self.tokenize_input, height=40)
+        tokenize_button.pack(side='left', padx=(0, 10), pady=5, fill='x', expand=True)
 
         # Clear button
-        clear_button = ctk.CTkButton(button_frame, text="Clear", command=self.clear_output,height=40)
-        clear_button.pack(side='right', pady=5,fill='x',expand='true')
+        clear_button = ctk.CTkButton(button_frame, text="Clear", command=self.clear_output, height=40)
+        clear_button.pack(side='right', pady=5, fill='x', expand=True)
 
         # Output text area
         output_label = ctk.CTkLabel(self.root, text="Tokens:")
@@ -181,6 +177,9 @@ class App:
         self.output.pack(padx=10, pady=10, fill="both", expand=True)
 
     def tokenize_input(self):
+        """
+        Tokenizes the SQL statement from the input text area and displays the tokens in the output area.
+        """
         sql = self.sql_entry.get("1.0", "end-1c")
         tokens = self.tokenizer.tokenize(sql)
         self.output.delete("1.0", ctk.END)
@@ -188,12 +187,17 @@ class App:
             self.output.insert(ctk.END, f"{token_type}: {value}\n")
 
     def clear_output(self):
+        """
+        Clears the input and output text areas.
+        """
         self.output.delete("1.0", ctk.END)
         self.sql_entry.delete("1.0", ctk.END)
 
     def run(self):
         self.root.mainloop()
 
-# Run the GUI
-app = App()
-app.run()
+
+# Start the application
+if __name__ == "__main__":
+    app = App()
+    app.run()
